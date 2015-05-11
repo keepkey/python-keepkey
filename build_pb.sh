23da5047f5a8dafffcb19f1df4de56f20b4cbc33
Darin Stanchfield <darin@keepkey.com>
2015-05-11 12:24:09 -0700

    changed location of proto bufs

--- build_pb.sh
+++ build_pb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURDIR=$(pwd)
 
-cd $CURDIR/../keepkey-firmware/interface/public
+cd $CURDIR/../device-protocol
 
 for i in messages types ; do
     protoc --python_out=$CURDIR/trezorlib/ -I/usr/include -I. $i.proto
