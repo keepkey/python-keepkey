471182fcf1a990918b06dfbfe8dc232721d1f548
Philip Khoo <philip@keepkey.com>
2016-07-26 13:56:51 -0700

    Updated exchange test along with protobuf interface files...

--- keepkeylib/exchange_pb2.py
+++ keepkeylib/exchange_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='exchange.proto',
   package='',
-  serialized_pb='\n\x0e\x65xchange.proto\"H\n\x0f\x45xchangeAddress\x12\x0f\n\x07\x61\x64\x64ress\x18\x01 \x01(\t\x12\x10\n\x08\x64\x65st_tag\x18\x02 \x01(\t\x12\x12\n\nrs_address\x18\x03 \x01(\t\"\xce\x01\n\x0f\x45xchangeRequest\x12\x19\n\x11withdrawal_amount\x18\x01 \x01(\x04\x12,\n\x12withdrawal_address\x18\x02 \x01(\x0b\x32\x10.ExchangeAddress\x12\x1c\n\x14withdrawal_coin_type\x18\x03 \x01(\t\x12\x19\n\x11\x64\x65posit_coin_type\x18\x04 \x01(\t\x12(\n\x0ereturn_address\x18\x05 \x01(\x0b\x32\x10.ExchangeAddress\x12\x0f\n\x07\x61pi_key\x18\x06 \x01(\x0c\"\xb4\x01\n\x10\x45xchangeResponse\x12!\n\x07request\x18\x01 \x01(\x0b\x32\x10.ExchangeRequest\x12)\n\x0f\x64\x65posit_address\x18\x02 \x01(\x0b\x32\x10.ExchangeAddress\x12\x16\n\x0e\x64\x65posit_amount\x18\x03 \x01(\x04\x12\x12\n\nexpiration\x18\x04 \x01(\r\x12\x13\n\x0bquoted_rate\x18\x05 \x01(\r\x12\x11\n\tsignature\x18\x06 \x01(\x0c\x42.\n\x1b\x63om.keepkey.device-protocolB\x0fKeepKeyExchange')
+  serialized_pb='\n\x0e\x65xchange.proto\"[\n\x0f\x45xchangeAddress\x12\x11\n\tcoin_type\x18\x01 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x02 \x01(\t\x12\x10\n\x08\x64\x65st_tag\x18\x03 \x01(\t\x12\x12\n\nrs_address\x18\x04 \x01(\t\"\xa7\x02\n\x10\x45xchangeResponse\x12)\n\x0f\x64\x65posit_address\x18\x01 \x01(\x0b\x32\x10.ExchangeAddress\x12\x16\n\x0e\x64\x65posit_amount\x18\x02 \x01(\x04\x12\x12\n\nexpiration\x18\x03 \x01(\r\x12\x13\n\x0bquoted_rate\x18\x04 \x01(\x04\x12,\n\x12withdrawal_address\x18\x05 \x01(\x0b\x32\x10.ExchangeAddress\x12\x19\n\x11withdrawal_amount\x18\x06 \x01(\x04\x12(\n\x0ereturn_address\x18\x07 \x01(\x0b\x32\x10.ExchangeAddress\x12\x0f\n\x07\x61pi_key\x18\x08 \x01(\x0c\x12\x11\n\tminer_fee\x18\t \x01(\x04\x12\x10\n\x08order_id\x18\n \x01(\x0c\"P\n\x16SignedExchangeResponse\x12#\n\x08response\x18\x01 \x01(\x0b\x32\x11.ExchangeResponse\x12\x11\n\tsignature\x18\x02 \x01(\x0c\x42.\n\x1b\x63om.keepkey.device-protocolB\x0fKeepKeyExchange')
 
 
 
@@ -26,22 +26,29 @@ _EXCHANGEADDRESS = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='address', full_name='ExchangeAddress.address', index=0,
+      name='coin_type', full_name='ExchangeAddress.coin_type', index=0,
       number=1, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=unicode("", "utf-8"),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='dest_tag', full_name='ExchangeAddress.dest_tag', index=1,
+      name='address', full_name='ExchangeAddress.address', index=1,
       number=2, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=unicode("", "utf-8"),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='rs_address', full_name='ExchangeAddress.rs_address', index=2,
+      name='dest_tag', full_name='ExchangeAddress.dest_tag', index=2,
       number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='rs_address', full_name='ExchangeAddress.rs_address', index=3,
+      number=4, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=unicode("", "utf-8"),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -56,70 +63,7 @@ _EXCHANGEADDRESS = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=18,
-  serialized_end=90,
-)
-
-
-_EXCHANGEREQUEST = _descriptor.Descriptor(
-  name='ExchangeRequest',
-  full_name='ExchangeRequest',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='withdrawal_amount', full_name='ExchangeRequest.withdrawal_amount', index=0,
-      number=1, type=4, cpp_type=4, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    _descriptor.FieldDescriptor(
-      name='withdrawal_address', full_name='ExchangeRequest.withdrawal_address', index=1,
-      number=2, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    _descriptor.FieldDescriptor(
-      name='withdrawal_coin_type', full_name='ExchangeRequest.withdrawal_coin_type', index=2,
-      number=3, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    _descriptor.FieldDescriptor(
-      name='deposit_coin_type', full_name='ExchangeRequest.deposit_coin_type', index=3,
-      number=4, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=unicode("", "utf-8"),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    _descriptor.FieldDescriptor(
-      name='return_address', full_name='ExchangeRequest.return_address', index=4,
-      number=5, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    _descriptor.FieldDescriptor(
-      name='api_key', full_name='ExchangeRequest.api_key', index=5,
-      number=6, type=12, cpp_type=9, label=1,
-      has_default_value=False, default_value="",
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  options=None,
-  is_extendable=False,
-  extension_ranges=[],
-  serialized_start=93,
-  serialized_end=299,
+  serialized_end=109,
 )
 
 
@@ -131,43 +75,71 @@ _EXCHANGERESPONSE = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='request', full_name='ExchangeResponse.request', index=0,
+      name='deposit_address', full_name='ExchangeResponse.deposit_address', index=0,
       number=1, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='deposit_address', full_name='ExchangeResponse.deposit_address', index=1,
-      number=2, type=11, cpp_type=10, label=1,
+      name='deposit_amount', full_name='ExchangeResponse.deposit_amount', index=1,
+      number=2, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='expiration', full_name='ExchangeResponse.expiration', index=2,
+      number=3, type=13, cpp_type=3, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='quoted_rate', full_name='ExchangeResponse.quoted_rate', index=3,
+      number=4, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='withdrawal_address', full_name='ExchangeResponse.withdrawal_address', index=4,
+      number=5, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='deposit_amount', full_name='ExchangeResponse.deposit_amount', index=2,
-      number=3, type=4, cpp_type=4, label=1,
+      name='withdrawal_amount', full_name='ExchangeResponse.withdrawal_amount', index=5,
+      number=6, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='expiration', full_name='ExchangeResponse.expiration', index=3,
-      number=4, type=13, cpp_type=3, label=1,
+      name='return_address', full_name='ExchangeResponse.return_address', index=6,
+      number=7, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='api_key', full_name='ExchangeResponse.api_key', index=7,
+      number=8, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value="",
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='miner_fee', full_name='ExchangeResponse.miner_fee', index=8,
+      number=9, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='quoted_rate', full_name='ExchangeResponse.quoted_rate', index=4,
-      number=5, type=13, cpp_type=3, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None),
-    _descriptor.FieldDescriptor(
-      name='signature', full_name='ExchangeResponse.signature', index=5,
-      number=6, type=12, cpp_type=9, label=1,
+      name='order_id', full_name='ExchangeResponse.order_id', index=9,
+      number=10, type=12, cpp_type=9, label=1,
       has_default_value=False, default_value="",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -181,17 +153,52 @@ _EXCHANGERESPONSE = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=302,
-  serialized_end=482,
+  serialized_start=112,
+  serialized_end=407,
 )
 
-_EXCHANGEREQUEST.fields_by_name['withdrawal_address'].message_type = _EXCHANGEADDRESS
-_EXCHANGEREQUEST.fields_by_name['return_address'].message_type = _EXCHANGEADDRESS
-_EXCHANGERESPONSE.fields_by_name['request'].message_type = _EXCHANGEREQUEST
+
+_SIGNEDEXCHANGERESPONSE = _descriptor.Descriptor(
+  name='SignedExchangeResponse',
+  full_name='SignedExchangeResponse',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='response', full_name='SignedExchangeResponse.response', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='signature', full_name='SignedExchangeResponse.signature', index=1,
+      number=2, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value="",
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  serialized_start=409,
+  serialized_end=489,
+)
+
 _EXCHANGERESPONSE.fields_by_name['deposit_address'].message_type = _EXCHANGEADDRESS
+_EXCHANGERESPONSE.fields_by_name['withdrawal_address'].message_type = _EXCHANGEADDRESS
+_EXCHANGERESPONSE.fields_by_name['return_address'].message_type = _EXCHANGEADDRESS
+_SIGNEDEXCHANGERESPONSE.fields_by_name['response'].message_type = _EXCHANGERESPONSE
 DESCRIPTOR.message_types_by_name['ExchangeAddress'] = _EXCHANGEADDRESS
-DESCRIPTOR.message_types_by_name['ExchangeRequest'] = _EXCHANGEREQUEST
 DESCRIPTOR.message_types_by_name['ExchangeResponse'] = _EXCHANGERESPONSE
+DESCRIPTOR.message_types_by_name['SignedExchangeResponse'] = _SIGNEDEXCHANGERESPONSE
 
 class ExchangeAddress(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
@@ -199,17 +206,17 @@ class ExchangeAddress(_message.Message):
 
   # @@protoc_insertion_point(class_scope:ExchangeAddress)
 
-class ExchangeRequest(_message.Message):
-  __metaclass__ = _reflection.GeneratedProtocolMessageType
-  DESCRIPTOR = _EXCHANGEREQUEST
-
-  # @@protoc_insertion_point(class_scope:ExchangeRequest)
-
 class ExchangeResponse(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
   DESCRIPTOR = _EXCHANGERESPONSE
 
   # @@protoc_insertion_point(class_scope:ExchangeResponse)
+
+class SignedExchangeResponse(_message.Message):
+  __metaclass__ = _reflection.GeneratedProtocolMessageType
+  DESCRIPTOR = _SIGNEDEXCHANGERESPONSE
+
+  # @@protoc_insertion_point(class_scope:SignedExchangeResponse)
 
 
 DESCRIPTOR.has_options = True
