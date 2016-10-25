7cb52819128f574c70ad7871930921d8bee31d08
Philip <philip@keepkey.com>
2016-10-25 16:16:42 -0700

    Removed ECDHSession key and set u2f counter options...

--- keepkeylib/client.py
+++ keepkeylib/client.py
@@ -606,15 +606,6 @@ class ProtocolMixin(object):
     def sign_identity(self, identity, challenge_hidden, challenge_visual, ecdsa_curve_name=DEFAULT_CURVE):
         return self.call(proto.SignIdentity(identity=identity, challenge_hidden=challenge_hidden, challenge_visual=challenge_visual, ecdsa_curve_name=ecdsa_curve_name))
 
-    @expect(proto.ECDHSessionKey)
-    def get_ecdh_session_key(self, identity, peer_public_key, ecdsa_curve_name=DEFAULT_CURVE):
-        return self.call(proto.GetECDHSessionKey(identity=identity, peer_public_key=peer_public_key, ecdsa_curve_name=ecdsa_curve_name))
-
-    @field('message')
-    @expect(proto.Success)
-    def set_u2f_counter(self, u2f_counter):
-        ret = self.call(proto.SetU2FCounter(u2f_counter = u2f_counter))
-        return ret
 
     def verify_message(self, address, signature, message):
         # Convert message to UTF8 NFC (seems to be a bitcoin-qt standard)
