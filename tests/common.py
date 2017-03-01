d2489a4f47e741574d3c4ef97524e4bbd7f4ebcd
Philip <philip@keepkey.com>
2017-03-01 10:22:26 -0800

    Added license header.

--- tests/common.py
+++ tests/common.py
@@ -15,6 +15,8 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
+#
+# The script has been modified for KeepKey Device.
 
 from __future__ import print_function
 
