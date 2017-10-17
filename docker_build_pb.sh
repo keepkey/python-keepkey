fbe9b8d388915b2c3c5188afddd9514f36021d39
Nick Miller <nick@ngmiller.io>
2017-10-17 14:05:15 -0600

    device-protocol repo handling now done by main build script

--- docker_build_pb.sh
+++ docker_build_pb.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# expects this repo and `device-protocol` to be cloned at the same filesystem level
-TEMPDIR="../device-protocol"
 IMAGETAG=keepkey/firmware
 
 docker run -it --rm \
     -v $(pwd):/root/python-keepkey \
-    -v $(pwd)/$TEMPDIR:/root/device-protocol \
     -w /root/python-keepkey \
     $IMAGETAG \
     ./build_pb.sh
