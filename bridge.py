05d6eba17ed8ea50bc6dd87575ed2575288881dc
solipsis <david@keepkey.com>
2019-03-08 17:23:57 -0700

    tcp emulator forwarder with debug link support

--- bridge.py
+++ bridge.py
@@ -1,69 +1,39 @@
-# recieve tcp
-# send packet to bridge
-# bridge sends packet to emulator
 
-from flask import Flask, request, jsonify
+from flask import Flask, Response, request, jsonify
 app = Flask(__name__)
 
 import socket
 import json
 import binascii
 
-# TCP_IP = '127.0.0.1'
-# TCP_PORT = 5005
-# BUFFER_SIZE = 1024
-# MESSAGE = "Hello"
 
+PACKET_SIZE = 64
 
+device = ('0.0.0.0', 21324)
+debug = ('0.0.0.0', 21325)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(device)
 
-# device = ('0.0.0.0', 21324)
-# debug = ('0.0.0.0', 21325)
+@app.route('/exchange/<string:kind>', methods=['GET', 'POST'])
+def exchange(kind):
 
-# msg = bytes([63, 35, 35, 0, 1, 0, 0, 0, 13, 10, 5, 104, 101, 108, 108, 111, 16, 0, 24, 1, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# s.connect(device)
-
-# s.send(msg)
-
-# try:
-    # # (msg_type, datalen) = self._read_headers(FakeRead(self._raw_read))
-    # # print (msg_type
-    # data = s.recv(64)
-    # print("Data", data)
-# except Exception as e:
-#     print(e)
-
-@app.route('/device', methods=['GET', 'POST'])
-def device():
-
-    print(request.method)
+    kk = device
+    if kind == 'debug':
+        kk = debug
 
     if request.method == 'POST':
-        device = ('0.0.0.0', 21324)
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(device)
 
         content = request.get_json(silent=True)
-        print(content)
-
-        print(content["data"])
         msg = bytearray.fromhex(content["data"])
-
         s.send(msg)
 
-        data = s.recv(64)
-        return '{"data":"' + binascii.hexlify(data) + '"}'
+        return Response('{}', status=200, mimetype='application/json')
 
-    return '{"a":"error"}'
+    if request.method == 'GET':
+        data = s.recv(PACKET_SIZE)
+        body = '{"data":"' + binascii.hexlify(data) + '"}'
+        return Response(body, status=200, mimetype='application/json')
 
+    return Response('{}', status=404, mimetype='application/json')
 
-
-    
-
-
-# s.connect((TCP_IP, TCP_PORT))
-# s.send(MESSAGE)
-# data = s.recv(BUFFER_SIZE)
-# s.close()
 
