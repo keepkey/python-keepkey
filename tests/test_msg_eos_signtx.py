a99c7dbf76f67cdb7046605867788ab8b9e68939
keepkeyjon <jon@keepkey.com>
2019-04-28 10:41:11 -0600

    policy: make AdvancedMode more permissive

--- tests/test_msg_eos_signtx.py
+++ tests/test_msg_eos_signtx.py
@@ -609,23 +609,6 @@ class TestMsgEosSignTx(common.KeepKeyTest):
 
         self.assertEqual(binascii.hexlify(res.hash), "8e0accde9fb6529b5d72b4d9a9859e1dae0c6ae9a159bb1ea8c8f579f942c291")
 
-    def test_unknown_noadvanced(self):
-        self.setup_mnemonic_nopin_nopassphrase()
-        self.client.apply_policy('AdvancedMode', 0)
-
-        try:
-            self.client.eos_sign_tx_raw(
-                proto.EosSignTx(
-                    address_n=parse_path("m/44'/194'/0'/0/0"),
-                    chain_id=EOS_CHAIN_ID,
-                    header=self.header(),
-                    num_actions=1),
-                self.action_unknown('somecontract', 'someaction', binascii.unhexlify('AB' * 15)))
-        except Exception as e:
-            self.assertEndsWith(e.args[1], "Signing cancelled by user")
-        else:
-            self.assert_(False, "Negative test passed")
-
     def test_unknown_advanced(self):
         self.setup_mnemonic_nopin_nopassphrase()
         self.client.apply_policy('AdvancedMode', 1)
