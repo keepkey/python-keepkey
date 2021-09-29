d2bd31bb8478f527b9d2458ab13faaa0def018bd
markrypto <mark.sco@shapeshift.com>
2021-09-29 10:27:33 -0600

    revert setup

--- wbsetup.py
+++ wbsetup.py
@@ -1,9 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-#setup(console=['wait-serv.py'])
-setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    console = [{'script': "wait-serv.py"}],
-    zipfile = None,
-)
+setup(console=['wait-serv.py'])
